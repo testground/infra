e56dc0f0dd112c3f7e3f9ce86e0dd642942bdd45
Anton Evangelatov <anton.evangelatov@gmail.com>
2020-04-22 20:19:33 +0200

    configmap for .env.toml

--- k8s/testground-daemon/rebuild-and-redeploy-daemon.sh
+++ k8s/testground-daemon/rebuild-and-redeploy-daemon.sh
@@ -21,6 +21,7 @@ popd
 
 kubectl delete deployment testground-daemon || true
 kubectl delete service testground-daemon || true
+kubectl apply -f config-map-env-toml.yml
 kubectl apply -f service-account.yml
 kubectl apply -f role-binding.yml
 kubectl apply -f deployment.yml -f service.yml
