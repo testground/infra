c02f8631e1cc17fa7894be3ecc4b9054e47f39fb
Anton Evangelatov <anton.evangelatov@gmail.com>
2020-04-23 13:20:34 +0200

    use iptestground/platform:edge image

--- k8s/testground-daemon/rebuild-and-push.sh
+++ k8s/testground-daemon/rebuild-and-push.sh
@@ -12,16 +12,6 @@ err_report() {
 trap 'err_report $LINENO' ERR
 
 pushd $GOPATH/src/github.com/testground/testground
-
 docker build -t nonsens3/testground:daemon -f Dockerfile.daemon .
-
 docker push nonsens3/testground:daemon
-
 popd
-
-kubectl delete deployment testground-daemon || true
-kubectl delete service testground-daemon || true
-kubectl apply -f config-map-env-toml.yml
-kubectl apply -f service-account.yml
-kubectl apply -f role-binding.yml
-kubectl apply -f deployment.yml -f service.yml
