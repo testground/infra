c272ae21688b49b71d5a27e363adfef953e74388
Dek Tech <43587123+dektech@users.noreply.github.com>
2022-07-05 13:04:29 +0200

    Updating the infra sh scripts

--- k8s/ebs-terraform/variables.tf
+++ k8s/ebs-terraform/variables.tf
@@ -3,7 +3,8 @@ variable "aws_region" {}
 variable "aws_availability_zone" {}
 
 variable "default_tags" {
-  type = "map"
+  type = map
+  description = "change KubernetesCluster to fit your cluster name"
 
   default = {
     Name              = "taas-daemon-datadir-volume"
