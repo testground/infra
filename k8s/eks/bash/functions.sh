2337e9ab4fd74cf95a725cb0092f777be5c8b1e0
LudiSistemas <portalscg@gmail.com>
2022-08-29 22:09:25 +0200

    geting ready for more advanced logic

--- k8s/eks/bash/functions.sh
+++ k8s/eks/bash/functions.sh
@@ -16,9 +16,47 @@ deploy_tigera_operator() {
 } | tee -a ./log/$start-log/deploy_tigera_operator.log
 
 deploy_multus_ds() {
-    kubectl apply ./multus-cni/deployments/multus-daemonset.ym
+    kubectl apply ./multus-cni/deployments/multus-daemonset.yml
 } | tee -a ./log/$start-log/deploy_multus_ds.log
 
 clone_multus() {
     git clone https://github.com/k8snetworkplumbingwg/multus-cni.git
 } | tee -a ./log/$start-log/deploy_multus_ds.log
+
+apply_weave() {
+    kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+} | tee -a ./log/$start-log/apply_weave.log
+
+create_weave() {
+    kubectl create -f ./yaml/weave.yml
+} | tee -a ./log/$start-log/create_weave.log
+
+
+#Not used until we have aws vpc cni ready
+deploy_multus_cm_vpc_cni() {
+    kubectl create -f ./yaml/multus-cm-vpc-cni.yaml
+} | tee -a ./log/$start-log/deploy_multus_cm_vpc_cni.log
+
+deploy_vpc_weave_multusds() {
+    kubectl create -f ./yaml/vpc-weave-multusds.yaml
+} | tee -a ./log/$start-log/vpc_weave_multusds.log
+
+
+deploy_vpc_multus_cm_calico() {
+    kubectl create -f ./yaml/multus-cm-calico.yaml
+} | tee -a ./log/$start-log/deploy_vpc_multus_cm_calico.log
+
+deploy_cluster_role_binding() {
+    kubectl create clusterrolebinding service-reader --clusterrole=service-reader --serviceaccount=default:default
+    kubectl create -f ./yaml/clusterrolebinding.yml
+} | tee -a ./log/$start-log/deploy_cluster_role_binding.log
+
+weave_ip_tables_drop() {
+    kubectl create -f ./yaml/drop-weave-cm.yml
+    kubectl create -f ./yaml/drop-weave-ds.yml
+} | tee -a ./log/$start-log/weave_ip_tables_drop.log
+
+multus_soflink() {
+    kubectl create -f ./yaml/soflink-cm.yml
+    kubectl create -f ./yaml/drop-weave-ds.yml
+} | tee -a ./log/$start-log/multus_soflink.log
--- k8s/eks/testground_install.sh
+++ k8s/eks/testground_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e #lets make sure that exit code 1 drops the script
 start = $(date +"%Y-%m-%d-%T")
 source ./bash/functions.sh
 cat << "EOF"
@@ -24,27 +25,10 @@ if [[ "$CLUSTER_NAME" == "default" ]]
 else
   echo "Creating cluster with name: $CLUSTER_NAME "
   echo "Please note, this can take up to 20 minutes to complete." 
-  cluseter_created = 1
+  cluster_created = 1
   create_cluster
   echo "========================"
   echo ""
-fi
-
-if [[ "$CNI_COMBINATION" == "calico_weave" ]]
- then
-   echo "Calico - weave combination is selected."
-   echo "Removing aws_node DS"
-   remove_aws_node_ds
-   echo "Adding tigera operator"
-   add_tigera_operator
-   echo "Deploying tigera operator"
-   deploy_tigera_operato
-   echo "========================"
-else
-  echo "Invalid selecton in .env"  >> ./log/$start-log/erorr.log
-  echo "CNI_COMBINATION cant be $CNI_COMBINATION" >> ./log/$start-log/erorr.log
-  echo "Option are calico_weave or aws_vpc_cni_weave"
-  exit 1
 fi
 
 echo "Detecting if multus-cni repo exists...."
@@ -55,10 +39,60 @@ then
     echo "/usr/bin/printf "[\xE2\x9C\x94] Multus-CNI\n"" >> ./log/$start-log/deploy_multus_ds.log
     echo "Now deploying multus-cni DS"
     deploy_multus_ds
+    echo "========================"
 else
     echo "[X] Multus-CNI" >> ./log/$start-log/deploy_multus_ds.log
     echo "Now cloning Multus repository..."
     clone_multus
     echo "Now deploying multus-cni DS"
     deploy_multus_ds
+     echo "========================"
 fi
+
+if [[ "$CNI_COMBINATION" == "calico_weave" ]]
+ then
+   echo "Calico - weave combination is selected."
+   echo "Removing aws_node DS"
+   remove_aws_node_ds
+   echo "Adding tigera operator"
+   add_tigera_operator
+   echo "Deploying tigera operator"
+   deploy_tigera_operator
+   echo "========================"
+else
+  echo "Invalid selecton in .env"  >> ./log/$start-log/erorr.log
+  echo "CNI_COMBINATION cant be $CNI_COMBINATION" >> ./log/$start-log/erorr.log
+  echo "Option are calico_weave or aws_vpc_cni_weave"
+  echo "========================"
+  exit 1
+fi
+
+echo "Applying and creating weave network attachment"
+apply_weave
+create_weave
+echo "========================"
+
+if [[ "$CNI_COMBINATION" == "calico_weave" ]]
+ then
+  echo "Deploying calico and multus DS"
+  deploy_vpc_weave_multusds
+  deploy_vpc_multus_cm_calico
+  echo "========================"
+else
+  echo "Deploying multusds and selected cni failed" >> ./log/$start-log/erorr.log
+  echo "exiting" >> ./log/$start-log/erorr.log
+  echo "========================"
+  exit 1
+fi
+
+echo "Now setging role binding..."
+deploy_cluster_role_binding
+echo "========================"
+
+echo "Droping weave ip table rule..."
+weave_ip_tables_drop
+echo "========================"
+
+echo "Making multus softlink..."
+multus_soflink
+echo "========================"
