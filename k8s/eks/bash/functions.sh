f1407061d416357cbd5d03e35b50468fe3d46b5f
LudiSistemas <portalscg@gmail.com>
2022-09-20 19:37:14 +0200

    missed one

--- k8s/eks/bash/functions.sh
+++ k8s/eks/bash/functions.sh
@@ -164,7 +164,7 @@ aws_get_vpc_id(){
 aws_get_subnet_id(){ 
     aws_get_vpc_id
     upper_az=$(echo $AVAILABILITY_ZONE | tr '[:lower:]' '[:upper:]' |  tr -d \-)
-    subnet_id=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc_id" "Name=tag:Name,Values=eksctl-$CLUSTER_NAME-cluster/SubnetPublic$upper_az"  | jq  ".Subnets[] | select(.AvailabilityZone==\"$AVAILABILITY_ZONE\") | .SubnetId " | tr -d \" )
+    subnet_id=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc_id" "Name=tag:Name,Values=eksctl-$CLUSTER_NAME-cluster/SubnetPrivate$upper_az"  | jq  ".Subnets[] | select(.AvailabilityZone==\"$AVAILABILITY_ZONE\") | .SubnetId " | tr -d \" )
 }
 
 
