421529fdd78ed4d74f8dff0ba230ca757bc136e1
Dek Tech <43587123+dektech@users.noreply.github.com>
2022-10-13 16:38:12 +0200

    new images, redirecting stderr to file

--- k8s/eks/testground_install.sh
+++ k8s/eks/testground_install.sh
@@ -30,82 +30,82 @@ if [[ "$CLUSTER_NAME" == "" ]]
 else
   echo "Creating cluster with name: $CLUSTER_NAME "
   echo "Please note, this can take up to 20 minutes to complete." 
-  create_cluster >> $real_path/log/$start-log/cluster.log
+  create_cluster &>> $real_path/log/$start-log/cluster.log
   echo "========================"
   echo ""
 fi
 
 echo "Now deploying multus-cni daemonset"
-deploy_multus_ds >> $real_path/log/$start-log/cluster.log
+deploy_multus_ds &>> $real_path/log/$start-log/cluster.log
 echo "========================"
 echo ""
 echo "Deploying the weave CNI to the cluster and creating the weave NetworkAttachmentDefinition"
-deploy_weave_cni >> $real_path/log/$start-log/cluster.log
-create_weave_networkattachmentdefinition >> $real_path/log/$start-log/cluster.log
+deploy_weave_cni &>> $real_path/log/$start-log/cluster.log
+create_weave_networkattachmentdefinition &>> $real_path/log/$start-log/cluster.log
 echo "========================"
 echo ""
 echo "Now setting role binding..."
-deploy_cluster_role_binding >> $real_path/log/$start-log/cluster.log
+deploy_cluster_role_binding &>> $real_path/log/$start-log/cluster.log
 echo "========================"
 echo ""
 echo "Dropping weave iptables rule..."
-weave_ip_tables_drop >> $real_path/log/$start-log/cluster.log
+weave_ip_tables_drop &>> $real_path/log/$start-log/cluster.log
 echo "========================"
 echo ""
 echo "Making multus softlink..."
-multus_softlink >> $real_path/log/$start-log/cluster.log
+multus_softlink &>> $real_path/log/$start-log/cluster.log
 echo "========================"
 echo ""
 echo "Creating cluster config file with name $CLUSTER_NAME.yaml"
-make_cluster_config >> $real_path/log/$start-log/cluster.log
+make_cluster_config &>> $real_path/log/$start-log/cluster.log
 echo "========================"
 echo ""
 echo "Creating nodegroup now, this can also take some time..."
-create_node_group >> $real_path/log/$start-log/cluster.log
+create_node_group &>> $real_path/log/$start-log/cluster.log
 echo "========================"
 echo ""
 echo "Now configuring storage.."
 echo ""
 echo "Creating EFS (Elastic File System)..."
-aws_create_file_system >> $real_path/log/$start-log/cluster.log
+aws_create_file_system &>> $real_path/log/$start-log/cluster.log
 echo "EFS created with ID : $efs_fs_id "
 echo ""
 echo "Now extracting subnet group ID..."
-aws_get_subnet_id >> $real_path/log/$start-log/cluster.log
+aws_get_subnet_id &>> $real_path/log/$start-log/cluster.log
 echo "Subnet group ID: $subnet_id"
 echo ""
-aws_get_sg_id >> $real_path/log/$start-log/cluster.log
+aws_get_sg_id &>> $real_path/log/$start-log/cluster.log
 echo "Done."
 echo ""
 
 echo "Creating EFS mount point"
-aws_create_efs_mount_point >> $real_path/log/$start-log/cluster.log
+aws_create_efs_mount_point &>> $real_path/log/$start-log/cluster.log
 echo "Your EFS mountpoint DNS is: $efs_dns"
 echo ""
 echo "Now creating EFS configmap"
-create_cm_efs >> $real_path/log/$start-log/cluster.log
+create_cm_efs &>> $real_path/log/$start-log/cluster.log
 echo ""
 echo "Now creating EFS manifest"
-create_efs_manifest >> $real_path/log/$start-log/cluster.log
+create_efs_manifest &>> $real_path/log/$start-log/cluster.log
 
 echo "========================"
 echo ""
 echo ""
 
 echo "Creating EBS volume (Elastic Block Store)"
-aws_create_ebs >> $real_path/log/$start-log/cluster.log
+aws_create_ebs &>> $real_path/log/$start-log/cluster.log
 echo "EBS created with this volume ID: $ebs_volume "
 echo ""
 echo "Now making persistent volume on EKS"
-make_persistent_volume >> $real_path/log/$start-log/cluster.log
+make_persistent_volume &>> $real_path/log/$start-log/cluster.log
 echo "Done."
 echo "========================"
 echo ""
 echo "Setting up Redis..."
-helm_redis_add_repo >> $real_path/log/$start-log/cluster.log
+helm_redis_add_repo &>> $real_path/log/$start-log/cluster.log
 echo "Helm Redis repo added"
 echo "Now proceeding with Helm install"
-helm_infra_install_redis >> $real_path/log/$start-log/cluster.log
+helm_infra_install_redis &>> $real_path/log/$start-log/cluster.log
 echo "Redis installed"
 echo "========================"
 echo ""
@@ -113,21 +113,21 @@ echo "We will setup the testground daemon now."
 echo ""
 echo ""
 echo "Creating configmap..."
-tg_daemon_config_map >> $real_path/log/$start-log/cluster.log
+tg_daemon_config_map &>> $real_path/log/$start-log/cluster.log
 echo "Creating service account..."
-tg_daemon_service_account >> $real_path/log/$start-log/cluster.log
+tg_daemon_service_account &>> $real_path/log/$start-log/cluster.log
 echo "Creating role binding..."
-tg_daemon_role_binding >> $real_path/log/$start-log/cluster.log
+tg_daemon_role_binding &>> $real_path/log/$start-log/cluster.log
 echo "Creating the testground service.."
-tg_daemon_services >> $real_path/log/$start-log/cluster.log
+tg_daemon_services &>> $real_path/log/$start-log/cluster.log
 echo "Creating svc sync service..."
-tg_daemon_svc_sync_service >> $real_path/log/$start-log/cluster.log
+tg_daemon_svc_sync_service &>> $real_path/log/$start-log/cluster.log
 echo "Creating sync service deployment..."
-tg_daemon_sync_service >> $real_path/log/$start-log/cluster.log
+tg_daemon_sync_service &>> $real_path/log/$start-log/cluster.log
 echo "Creating sidecar..."
-tg_daemon_sidecar >> $real_path/log/$start-log/cluster.log
+tg_daemon_sidecar &>> $real_path/log/$start-log/cluster.log
 echo "Creating testground daemon deployment..."
-tg_daemon_deployment >> $real_path/log/$start-log/cluster.log
+tg_daemon_deployment &>> $real_path/log/$start-log/cluster.log
 
 echo "========================"
 echo ""
