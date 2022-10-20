ac1b35eda0f40c451cdd9d42c9ea6170a6a4316e
Dek Tech <43587123+dektech@users.noreply.github.com>
2022-10-20 13:21:34 +0200

    another path fix

--- k8s/eks/monitoring.sh
+++ k8s/eks/monitoring.sh
@@ -5,7 +5,7 @@ parentdir=$(cd $real_path && cd ../ && pwd)
 # install the prometheus operator
 kubectl create -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/master/bundle.yaml
 # add the prometheus config and install prometheus
-kubectl apply -f $real_path/yaml/prometheus/prom_rbac.yml
+kubectl apply -f $real_path/yaml/prometheus/prom-rbac.yml
 kubectl apply -f $real_path/yaml/prometheus/prometheus.yml
 # add weave monitoring dependencies
 kubectl apply -f $real_path/yaml/prometheus/weave-svc.yml
@@ -14,7 +14,7 @@ kubectl apply -f $real_path/yaml/prometheus/weave-svc-monitor.yml
 cd $parentdir/kops/testground-infra/charts/testground-dashboards
 helm install testground-infra .
 # add and install grafana
-cd $real_path/grafana
+cd $real_path/yaml/grafana
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 helm install grafana grafana/grafana
