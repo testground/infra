9260cf7d5577c829a1357e35834eaaecd2dd8068
Jose Ramon Mañes <jose@celestia.org>
2023-05-02 17:39:42 +0200

    some updates and fixes - cluster can be created now

--- k8s/kops/01_install_k8s.sh
+++ k8s/kops/01_install_k8s.sh
@@ -53,7 +53,7 @@ fi
 
 kops create -f $CLUSTER_SPEC
 kops create secret --name $CLUSTER_NAME sshpublickey admin -i $PUBKEY
-kops update cluster $CLUSTER_NAME --yes
+kops update cluster $CLUSTER_NAME --admin --yes
 
 # Wait for worker nodes and master to be ready
 kops validate cluster --wait 20m
@@ -74,28 +74,71 @@ kubectl apply -f ./kops-weave/weave.yml \
               -f ./kops-weave/dummy.yml \
               -f ./sidecar.yaml
 
-echo "Installing Prometheus"
-pushd prometheus-operator
-helm install prometheus-operator stable/prometheus-operator -f values.yaml
-popd
+echo "Installing ArgoCD"
+kubectl create namespace argocd
 
-echo "Installing InfluxDB"
-pushd influxdb
-helm install influxdb bitnami/influxdb -f ./values.yaml
-popd
+helm repo add argo https://argoproj.github.io/argo-helm
+helm install argocd argo/argo-cd --version 5.31.0 \
+  --set redis-ha.enabled=true \
+  --set controller.replicas=1 \
+  --set server.autoscaling.enabled=true \
+  --set server.autoscaling.minReplicas=2 \
+  --set repoServer.autoscaling.enabled=true \
+  --set repoServer.autoscaling.minReplicas=2 \
+  --set applicationSet.replicaCount=2
 
+#kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sleep 30
 
-echo "Installing Redis and Grafana dashboards"
-pushd testground-infra
-helm dep build
-helm install testground-infra .
-popd
+echo "Installing root app..."
+kubectl apply -f - <<EOF
+# We generate this ArgoCD application with Terraform, but we keep it here as a workaround
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: root
+  namespace: argocd
+spec:
+  project: default
+  source:
+    repoURL: https://github.com/celestiaorg/testground-infra.git
+    path: 'argocd'
+    targetRevision: jose/hackground-k8s-tf
+  destination:
+    name: in-cluster
+    namespace: argocd
+  syncPolicy:
+    automated:
+      prune: true
+      allowEmpty: true
+      selfHeal: true
+    syncOptions:
+    - ApplyOutOfSyncOnly=true
+    - CreateNamespace=true
+EOF
 
-echo "Install Weave service monitor..."
-echo
+#echo "Installing Prometheus"
+#pushd prometheus-operator
+#helm install prometheus-operator stable/prometheus-operator -f values.yaml
+#popd
+#
+#echo "Installing InfluxDB"
+#pushd influxdb
+#helm install influxdb bitnami/influxdb -f ./values.yaml
+#popd
+#
+#
+#echo "Installing Redis and Grafana dashboards"
+#pushd testground-infra
+#helm dep build
+#helm install testground-infra .
+#popd
 
-kubectl apply -f ./kops-weave/weave-metrics-service.yml \
-              -f ./kops-weave/weave-service-monitor.yml
+#echo "Install Weave service monitor..."
+#echo
+#
+#kubectl apply -f ./kops-weave/weave-metrics-service.yml \
+#              -f ./kops-weave/weave-service-monitor.yml
 
 echo "Wait for Sidecar to be Ready..."
 echo
