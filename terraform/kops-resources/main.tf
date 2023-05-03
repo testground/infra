2d9cf0c9dbb00bb691b27be4c71c636a49dc0875
Jose Ramon Mañes <jose@celestia.org>
2023-05-03 12:37:24 +0200

    feat: add resources required when creating a cluster with kops

---
+++ terraform/kops-resources/main.tf
@@ -0,0 +1,33 @@
+//################################################################################
+//# Locals
+//################################################################################
+locals {
+  region       = "eu-west-1"
+  project_name = "jose-k8s-local"
+
+  azs                         = ["${local.region}a", "${local.region}b"]
+  vpc_id                      = data.aws_vpc.selected.id
+  vpc_cidr                    = "${split(".", data.aws_vpc.selected.cidr_block)[0]}.${split(".", data.aws_vpc.selected.cidr_block)[1]}"
+  private_subnets             = [for i in data.aws_subnet_ids.private_subnets.ids : i]
+  private_subnets_cidr_blocks = [for i in data.aws_subnet.aws_subnet_ids : i.cidr_block]
+
+  ebs_types = ["gp2", "gp3"]
+}
+
+data "aws_vpc" "selected" {
+  tags = {
+    Name = replace("${local.project_name}", "-", ".")
+  }
+}
+
+data "aws_subnet_ids" "private_subnets" {
+  vpc_id = data.aws_vpc.selected.id
+  tags = {
+    KubernetesCluster = replace("${local.project_name}", "-", ".")
+  }
+}
+
+data "aws_subnet" "aws_subnet_ids" {
+  for_each = toset(data.aws_subnet_ids.private_subnets.ids)
+  id       = each.value
+}
---
+++ terraform/kops-resources/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 0.13.1"
+}
+
+provider "aws" {
+  region = local.region
+}
+
+# https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/guides/getting-started#provider-setup
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
