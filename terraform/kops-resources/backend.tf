3055f3f59b769aa7d7395fb5c5bd6e8628faef51
Jose Ramon Mañes <jose@celestia.org>
2023-05-04 13:33:27 +0200

    feat: remove efs roles

--- terraform/kops-resources/backend.tf
+++ terraform/kops-resources/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "testground-terraform-state"
-    key    = "jose.k8s.local/tf-state-devops-tg"
+    key    = "jose.k8s.local-tf-state-devops-tg"
     region = "eu-west-1"
   }
 }
