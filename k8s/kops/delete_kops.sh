e5f2c8877ebf8987206e0f556609290610ca4e4d
Jose Ramon Mañes <jose@celestia.org>
2023-05-05 11:55:31 +0200

    feat: add delete tf resources + cluster

--- k8s/kops/delete_kops.sh
+++ k8s/kops/delete_kops.sh
@@ -5,10 +5,19 @@ set -o pipefail
 set -x
 set -e
 
+TF_RESOURCES="../../terraform/kops-resources/"
+
 err_report() {
     echo "Error on line $1"
 }
 
 trap 'err_report $LINENO' ERR
 
+# =======================================================
+echo "Create EFS resources & storageclasses..."
+cd ${TF_RESOURCES}
+terraform destroy -auto-approve &&\
+cd -
+pwd
+# =======================================================
 kops delete cluster $CLUSTER_NAME --yes
