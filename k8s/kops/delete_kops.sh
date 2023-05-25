30d2e90c9d8a742410df5b3e92e20f6b8248c019
Jose Ramon Mañes <jose@celestia.org>
2023-05-25 13:34:40 +0200

    feat: load vars from delete.sh

--- k8s/kops/delete_kops.sh
+++ k8s/kops/delete_kops.sh
@@ -5,13 +5,25 @@ set -o pipefail
 set -x
 set -e
 
+# =======================================================
 TF_RESOURCES="../../terraform/kops-resources/"
+# =======================================================
 
 err_report() {
     echo "Error on line $1"
 }
 
 trap 'err_report $LINENO' ERR
+
+# =======================================================
+
+# Load values form the file
+if [ -f ./values.sh ];then
+    source ./values.sh
+else
+    echo "[ERROR] Please, you need to fill the values in the script [values.sh]"
+    exit 1
+fi
 
 # =======================================================
 echo "Create EFS resources & storageclasses..."
